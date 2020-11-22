4fcf7101e43d66fa0e56c3a447362a7ea600a6ba
Douglas Vitor <douglas_oliveiravitor@hotmail.com>
2020-11-22 20:02:22 -0300

    Atualizar metodo de incluir para editar também

--- agenda.py
+++ agenda.py
@@ -26,16 +26,18 @@ def buscar_contato(contato):
     print('-------------------------------------------\n')
 
 
-def incluir_contato(contato, telefone, email, endereco):
+def incluir_editar_contato(contato, telefone, email, endereco):
     AGENDA[contato] = {
         'telefone': telefone,
         'email': email,
         'endereco': endereco,
     }
-    print('\n>>> Contato {} adicionado com sucesso! <<<\n'.format(contato))
+    print('\n>>> Contato {} adicionado/editado com sucesso! <<<\n'.format(contato))
 
 
-mostrar_contatos()
+#mostrar_contatos()
 #buscar_contato('Maria')
-incluir_contato('Joao', '9993888811', 'joao@solyd.com.br', 'Av. 3')
+incluir_editar_contato('Joao', '9993888811', 'joao@solyd.com.br', 'Av. 3')
+incluir_editar_contato('Guilherme', '8888888888', 'guigui@solyd.com.br', 'Av. 1')
+incluir_editar_contato('José', '777777777', None, None)
 mostrar_contatos()
