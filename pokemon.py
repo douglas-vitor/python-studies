e2e0f2a24742a253d7b8cc6847ef7aaa089fc95c
Douglas Vitor <douglas_oliveiravitor@hotmail.com>
2020-11-24 16:02:42 -0300

    Definindo argumentos não obrigatórios

--- pokemon.py
+++ pokemon.py
@@ -1,10 +1,16 @@
 class Pokemon:
-    def __init__(self, tipo, especie):
+    def __init__(self, tipo, especie, level=1, nome=None):
         self.tipo = tipo
         self.especie = especie
+        self.level = level
+
+        if nome:
+            self.nome = nome
+        else:
+            self.nome = especie
 
     def __str__(self):
-        return '{} ({})'.format(self.especie, self.tipo)
+        return '{}({})'.format(self.nome, self.level)
 
     def atacar(self, pokemon):
         print('{} atacou {}!'.format(self, pokemon))
