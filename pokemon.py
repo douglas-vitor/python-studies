d4acc70f065e04a4b22416c7521b9201d4092f83
Douglas Vitor <douglas_oliveiravitor@hotmail.com>
2020-11-24 19:39:42 -0300

    Adicionado novas classes

--- pokemon.py
+++ pokemon.py
@@ -1,6 +1,5 @@
 class Pokemon:
-    def __init__(self, tipo, especie, level=1, nome=None):
-        self.tipo = tipo
+    def __init__(self, especie, level=1, nome=None):
         self.especie = especie
         self.level = level
 
@@ -15,7 +14,21 @@ class Pokemon:
     def atacar(self, pokemon):
         print('{} atacou {}!'.format(self, pokemon))
 
-meupok = Pokemon('eletrico', 'pikachu')
-inimipok = Pokemon('fogo', 'charizard')
 
-meupok.atacar(inimipok)
+class PokemonEletrico(Pokemon):
+    tipo = 'eletrico'
+    def atacar(self, pokemon):
+        print('{} lançou um raio do trovão em {}!'.format(self, pokemon))
+
+class PokemonFogo(Pokemon):
+    tipo = 'fogo'
+    def atacar(self, pokemon):
+        print('{} lançou uma bola de fogo em {}!'.format(self, pokemon))
+
+class PokemonAgua(Pokemon):
+    tipo = 'agua'
+    def atacar(self, pokemon):
+        print('{} lançou um jato dagua em {}!'.format(self, pokemon))
+
+class Pikachu(PokemonEletrico):
+    especie = 'Pikachu'
