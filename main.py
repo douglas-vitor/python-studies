b74597aa57f6729710a54d77372e96624d4efec7
Douglas Vitor <douglas_oliveiravitor@hotmail.com>
2020-11-29 07:46:18 -0300

    Criado a gameplay inicial

--- main.py
+++ main.py
@@ -28,11 +28,43 @@ def escolher_pokemon_inicial(player):
         else:
             print('Escolha inválida.')
 
-player = Player('Douglas')
-player.mostrar_dinheiro()
-player.capturar(PokemonEletrico('Pikachu', level=1))
+if __name__ == '__main__':
+    print('\n\|/-\|/-\|/-\|/-\|/-\|/-\|/-\|/-\|/-')
+    print('Bem-vindo ao game Pokemon RPG - Terminal Edition!!!')
+    print('\|/-\|/-\|/-\|/-\|/-\|/-\|/-\|/-\|/- \n')
+    nome = input('Olá, qual é o seu nome ?: ')
+    player = Player(nome)
+    print('Olá {}, este é um mundo terminal habitado por Pokemons, a partir de agora sua missão é se tornar um mestre dos Pokemons.'.format(nome))
+    print('Capture o máximo de Pokemons que conseguir e lute com seus inimigos.')
+    player.mostrar_dinheiro()
 
-'''inimigo1 = Inimigo(pokemons=[PokemonAgua('Squirtle', level=1)])
-player.batalhar(inimigo1)'''
+    if player.pokemons:
+        print('Já vi que você tem alguns Pokemons.')
+        player.mostrar_pokemons()
+    else:
+        print('Você não tem nenhum Pokemon, portanto precisa escolher um Pokemon inicial.')
+        escolher_pokemon_inicial(player)
+    
+    print('Agora que você já possui um Pokemon, enfrente seu arquirrival Lawliet!')
+    lawliet = Inimigo(nome='Lawliet', pokemons=[PokemonAgua('Squirtle', level=1)])
+    player.batalhar(lawliet)
 
-player.explorar()
+    while True:
+        print('\n--------------------')
+        print('O que deseja fazer?')
+        print('1 - Explorar pelo mundo')
+        print('2 - Lutar com um inimigo')
+        print('0 - Sair do jogo')
+        print('--------------------\n')
+        escolha = input('Opção: ')
+
+        if escolha == '0':
+            print('Jogo encerrado.')
+            break
+        elif escolha == '1':
+            player.explorar()
+        elif escolha == '2':
+            inimigo_aleatorio = Inimigo()
+            player.batalhar(inimigo_aleatorio)
+        else:
+            print('Opção inválida.')
