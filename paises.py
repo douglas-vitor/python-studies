3f2d1024808269d5484a44447a24207cb09ee91a
Douglas Vitor <douglas_oliveiravitor@hotmail.com>
2020-12-10 13:39:05 -0300

    Parsing json

--- paises.py
+++ paises.py
@@ -1,7 +1,14 @@
+import json
+
 import requests
 
 #Documentação API : https://restcountries.eu/
-
 URL = "https://restcountries.eu/rest/v2/all"
+
 res = requests.get(URL)
-print(res.text)
+
+paises = json.loads(res.text)
+
+for pais in paises:
+    print(pais['name'])
+print(len(paises))
