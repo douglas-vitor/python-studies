90bc64839ac06e0949defe43acf2df7af06b089b
Douglas Vitor <douglas_oliveiravitor@hotmail.com>
2020-12-13 15:30:47 -0300

    Parsing html com BS4

--- crawler.py
+++ crawler.py
@@ -1,16 +1,30 @@
 import requests
 
+from bs4 import BeautifulSoup
+
+
 URL_AUTOMOVEIS = "https://django-anuncios.solyd.com.br/automoveis/"
 
 def buscar(url):
     try:
         res = requests.get(url)
         if res.status_code == 200:
-            print(res.text)
+            return res.text
         else:
             print("Status code error.")
     except Exception as err:
         print("Erro ao fazer requisição de busca.")
         print(err)
 
-buscar(URL_AUTOMOVEIS)
+
+def parsing(res_html):
+    try:
+        soup = BeautifulSoup(res_html, "html.parser")
+        return soup
+    except Exception as err:
+        print("Erro no parsing.")
+        print(err)
+
+resposta = buscar(URL_AUTOMOVEIS)
+if resposta:
+    soup = parsing(resposta)
