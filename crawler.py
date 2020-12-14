c0c8ea08ade5fc8dfa8bef413b74dcfeb8910079
Douglas Vitor <douglas_oliveiravitor@hotmail.com>
2020-12-14 19:09:00 -0300

    Encontrando os links

--- crawler.py
+++ crawler.py
@@ -3,6 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 
 
+DOMINIO = "https://django-anuncios.solyd.com.br"
 URL_AUTOMOVEIS = "https://django-anuncios.solyd.com.br/automoveis/"
 
 def buscar(url):
@@ -25,6 +26,21 @@ def parsing(res_html):
         print("Erro no parsing.")
         print(err)
 
+
+def encontrar_links(soup):
+    cards_pai = soup.find("div", class_="ui three doubling link cards")
+    cards = cards_pai.find_all("a")
+    links = []
+    for card in cards:
+        link = card['href']
+        links.append(link)
+    return links
+
+
 resposta = buscar(URL_AUTOMOVEIS)
 if resposta:
     soup = parsing(resposta)
+    if soup:
+        links = encontrar_links(soup)
+        for link in links:
+            print(link)
