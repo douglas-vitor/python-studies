17b09841ff6e040e0573d613d667ed93d3afaf78
Douglas Vitor <douglas_oliveiravitor@hotmail.com>
2021-01-10 08:21:25 -0300

    For no template

--- projeto6/Anuncios/models.py
+++ projeto6/Anuncios/models.py
@@ -6,6 +6,9 @@ class Categoria(models.Model):
     def __str__(self):
         return self.titulo
 
+    class Meta:
+        ordering = ['titulo']
+
 
 class Anuncio(models.Model):
     titulo = models.CharField(max_length=40)
--- projeto6/Anuncios/views.py
+++ projeto6/Anuncios/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Categoria
+
 def home(request):
-    return render(request, 'home.html')
+    categorias = Categoria.objects.all()
+    return render(request, 'home.html', {'categorias': categorias})
