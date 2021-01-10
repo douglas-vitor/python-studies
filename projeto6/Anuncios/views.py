f276783391056de2fbe1307f9a6668ae8c6a65a9
Douglas Vitor <douglas_oliveiravitor@hotmail.com>
2021-01-10 08:47:49 -0300

    Adicionando anuncios ao template

--- projeto6/Anuncios/views.py
+++ projeto6/Anuncios/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Categoria
+from .models import Anuncio
 
 def home(request):
     categorias = Categoria.objects.all()
-    return render(request, 'home.html', {'categorias': categorias})
+    anuncios = Anuncio.objects.all()
+
+    return render(request, 'home.html', {'categorias': categorias,
+    'anuncios': anuncios})
