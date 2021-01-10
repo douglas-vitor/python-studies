ae201fec9501a68b473275ca044b6ded9d02699e
Douglas Vitor <douglas_oliveiravitor@hotmail.com>
2021-01-10 08:53:46 -0300

    Filtrando ultimos anuncios

--- projeto6/Anuncios/views.py
+++ projeto6/Anuncios/views.py
@@ -6,7 +6,7 @@ from .models import Anuncio
 
 def home(request):
     categorias = Categoria.objects.all()
-    anuncios = Anuncio.objects.all()
+    ultimos_anuncios = Anuncio.objects.all()[:12]
 
     return render(request, 'home.html', {'categorias': categorias,
-    'anuncios': anuncios})
+    'anuncios': ultimos_anuncios})
