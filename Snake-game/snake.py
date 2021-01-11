3805df01570b0b48747971d320a624ae7298f8d1
Douglas Vitor <douglas_oliveiravitor@hotmail.com>
2021-01-11 18:42:15 -0300

    Criando a frutinha

--- Snake-game/snake.py
+++ Snake-game/snake.py
@@ -9,7 +9,21 @@ verde = (46, 139, 87)
 screen.fill(verde)
 pygame.display.update()
 
+class frutinha:
+    cor = (255, 0, 0)
+    posicao = (200, 200)
+    tamanho = (10, 10)
+
+    def __init__(self):
+        self.textura = pygame.Surface(self.tamanho)
+        self.textura.fill(self.cor)
+
+frutinha = frutinha()
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
+        
+    screen.blit(frutinha.textura, frutinha.posicao)
+    pygame.display.update()
