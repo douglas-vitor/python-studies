f8ad9552f0d86f34cf8492a8a7dc224c1e6b5f0a
Douglas Vitor <douglas_oliveiravitor@hotmail.com>
2021-01-11 19:00:39 -0300

    Criando a cobrinha

--- Snake-game/snake.py
+++ Snake-game/snake.py
@@ -11,7 +11,23 @@ verde = (46, 139, 87)
 screen.fill(verde)
 pygame.display.update()
 
-class frutinha:
+
+class Snake:
+    cor = (0, 0, 0)
+    tamanho = (10, 10)
+
+    def __init__(self):
+        self.textura = pygame.Surface(self.tamanho)
+        self.textura.fill(self.cor)
+
+        self.corpo = [(100, 100), (90, 100), (80, 100)]
+
+    def blit(self, screen):
+        for posicao in self.corpo:
+            screen.blit(self.textura, posicao)
+
+
+class Frutinha:
     cor = (139, 0, 0)
     tamanho = (10, 10)
 
@@ -23,13 +39,20 @@ class frutinha:
         y = random.randint(0, 49) * 10
         self.posicao = (x, y)
 
+    def blit(self, screen):
+        screen.blit(self.textura, self.posicao)
 
-frutinha = frutinha()
+
+
+frutinha = Frutinha()
+cobrinha = Snake()
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
         
-    screen.blit(frutinha.textura, frutinha.posicao)
+    frutinha.blit(screen)
+    cobrinha.blit(screen)
+    
     pygame.display.update()
