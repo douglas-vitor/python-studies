6a046729301a194d1f2e4b23c0521db3f4dcfb56
Douglas Vitor <douglas_oliveiravitor@hotmail.com>
2021-01-11 18:30:48 -0300

    Criando a tela inicial

--- Snake-game/snake.py
+++ Snake-game/snake.py
@@ -1,1 +1,15 @@
 import pygame
+
+pygame.init()
+
+resolucao = (500, 500)
+screen = pygame.display.set_mode(resolucao)
+
+verde = (46, 139, 87)
+screen.fill(verde)
+pygame.display.update()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
